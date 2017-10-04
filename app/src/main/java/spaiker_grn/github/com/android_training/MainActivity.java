f03d4c81c4ca557557014723b593538dcc34f26c
DESKTOP-A2BQ3A0\Дмитрий <spaiker.grn@gmail.com>
2017-10-04 11:57:51 +0300

    cherry pick master to feature 2

--- app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
+++ app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
@@ -17,5 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
     }
 }
