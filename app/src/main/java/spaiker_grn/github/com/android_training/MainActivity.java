234938efa92e12c2be30577df3189b6b5d4ab2ef
DESKTOP-A2BQ3A0\Дмитрий <spaiker.grn@gmail.com>
2017-10-04 11:50:03 +0300

    create project

---
+++ app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
@@ -0,0 +1,13 @@
+package spaiker_grn.github.com.android_training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
