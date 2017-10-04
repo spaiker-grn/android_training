a165d4881f9c4a0fa8e6c66a412dcbeaed322783
DESKTOP-A2BQ3A0\Дмитрий <spaiker.grn@gmail.com>
2017-10-04 11:57:51 +0300

    added some filed

--- app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
+++ app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
@@ -2,12 +2,18 @@ package spaiker_grn.github.com.android_training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
     }
 }
