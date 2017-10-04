89a7d7f7ba0d6208881da8975ea95a1b83754374
DESKTOP-A2BQ3A0\Дмитрий <spaiker.grn@gmail.com>
2017-10-04 12:05:33 +0300

    implements View.OnClickListener

--- app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
+++ app/src/main/java/spaiker_grn/github/com/android_training/MainActivity.java
@@ -2,11 +2,17 @@ package spaiker_grn.github.com.android_training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onClick(final View v) {
+
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
